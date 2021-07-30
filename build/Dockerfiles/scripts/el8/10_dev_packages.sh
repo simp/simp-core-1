0d6fe6841b8778ed63e803e0a82d58880b6f4d52
Chris Tessmer <chris.tessmer@onyxpoint.com>
2021-07-30 23:30:08 +0000

    Update EL8 Dockerfile to support simp-doc builds

--- build/Dockerfiles/scripts/el8/10_dev_packages.sh
+++ build/Dockerfiles/scripts/el8/10_dev_packages.sh
@@ -1,17 +1,20 @@
 #!/bin/sh -e
 
 # Install necessary packages
-yum install -y epel-release
-yum install -y rpm-build rpmdevtools ruby-devel rpm-devel rpm-sign
-yum install -y util-linux openssl augeas-libs createrepo genisoimage git gnupg2 libicu-devel libxml2 libxml2-devel libxslt libxslt-devel which ruby-devel
-yum -y install scl-utils python2-virtualenv python3-virtualenv fontconfig dejavu-sans-fonts dejavu-sans-mono-fonts dejavu-serif-fonts dejavu-fonts-common libjpeg-devel zlib-devel openssl-devel
-yum install -y libyaml glibc-headers autoconf gcc gcc-c++ glibc-devel readline-devel libffi-devel automake libtool bison sqlite-devel pinentry
+dnf install -y epel-release
+dnf install -y rpm-build rpmdevtools ruby-devel rpm-devel rpm-sign
+dnf install -y util-linux openssl augeas-libs createrepo genisoimage git gnupg2 libicu-devel libxml2 libxml2-devel libxslt libxslt-devel which ruby-devel
+dnf -y install scl-utils python2-virtualenv python3-virtualenv fontconfig dejavu-sans-fonts dejavu-sans-mono-fonts dejavu-serif-fonts dejavu-fonts-common libjpeg-devel zlib-devel openssl-devel
+dnf install -y libyaml glibc-headers autoconf gcc gcc-c++ glibc-devel readline-devel libffi-devel automake libtool bison sqlite-devel pinentry
 
 # Install helper packages
-yum install -y rubygems vim-enhanced jq
+dnf install -y rubygems vim-enhanced jq
+
+# Install for python/simp-doc pkg:build
+dnf install -y dejavu-fonts-common dejavu-sans-fonts dejavu-sans-mono-fonts dejavu-serif-fonts fontconfig libjpeg-devel python3-pip python3-virtualenv zlib-devel
 
 # Install SSH for CI testing
-yum -y install initscripts
+dnf -y install initscripts
 if [ -d /etc/ssh ]; then /bin/cp -a /etc/ssh /root; fi
-yum -y install openssh-server
+dnf -y install openssh-server
 if [ -d /root/ssh ]; then /bin/cp -a /root/ssh /etc && /bin/rm -rf /root/ssh; fi
